2c789fa962cf43401e43a297975dc9dfb4a655de
Julien Van Tongerloo <julien.vantongerloo@intuitim.com>
2022-11-18 09:04:35 +0100

    add - perf.sh

--- perf.sh
+++ perf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#arg: dossier
+#arg: file
 
-make -C $1 -s clean
+make -s clean
+make -s $1
 
 echo "n_threads,time1,time2,time3,time4,time5" > measure.csv
 
@@ -9,9 +10,8 @@ for i in 1 8 16 32 64; do
 	echo -n "$1" >> measure.csv
 	for _ in {1...5}; do
 		echo "Essai $j avec $i threads"
-		time_val=$(/usr/bin/time -f %e make -C $1 -j $i -s all >/dev/null | tail -1)
+		time_val=$(/usr/bin/time -f %e $1 -j $i > /dev/null | tail -1)
 		echo -n ",$time_val" >> measure.csv
-		make -C $1 -s clean
 	done
 	echo "" >> measure.csv
 done
