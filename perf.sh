92f488790f20501ab3ae681de20ee7f3b6b8d18e
Julien Van Tongerloo <julien.vantongerloo@intuitim.com>
2022-11-18 10:47:19 +0100

    add - read-write

--- perf.sh
+++ perf.sh
@@ -7,7 +7,7 @@ make -s $1 2> /dev/null
 echo "n_threads,time1,time2,time3,time4,time5" > measure.csv
 
 for i in 1 8 16 32 64; do
-	echo -n "$1" >> measure.csv
+	echo -n "$i" >> measure.csv
 	for _ in {1...5}; do
 		echo "Essai $j avec $i threads"
 		time_val=$(/usr/bin/time -f %e $1 -r $i -w $i > /dev/null | tail -1)
