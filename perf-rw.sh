b73a573213992000e4ac751046f4f0ae0cce675f
root <julien.vantongerloo@hotmail.com>
2022-11-21 12:32:40 +0100

    refactor - dynamic shell script

--- perf-rw.sh
+++ perf-rw.sh
@@ -4,13 +4,19 @@
 make -s clean
 make -s $1 2> /dev/null
 
-echo "n_threads,time1,time2,time3,time4,time5" > measure.csv
+if [ $1 = "read-write" ]; then
+	file="measure-rw.csv"
+fi
+
+echo "n_threads,time1,time2,time3,time4,time5" > $file
 
 for i in 1 8 16 32 64; do
-	echo -n "$i" >> measure.csv
+	echo -n "$i" >> $file
 	for j in 1 2 3 4 5; do
-		timeval=`/usr/bin/time -f %e $1 -r $i -w $i 2>&1`
-		echo -n ",$timeval" >> measure.csv
+		if [ $1 = "read-write" ]; then
+			timeval=`/usr/bin/time -f %e ./$1 -r $i -w $i 2>&1`
+		fi
+		echo -n ",$timeval" >> $file
 	done
-	echo "" >> measure.csv
+	echo "" >> $file
 done
