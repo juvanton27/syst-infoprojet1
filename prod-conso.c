c5d9abec6474da26a762921f85578c7896a38fed
jef.overweg <jef.overweg@student.vinci.be>
2022-11-24 12:42:13 +0100

    l

--- prod-conso.c
+++ prod-conso.c
@@ -1,4 +1,33 @@
+#include <pthread.h>
 
-int main() {
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdbool.h>
+
+#include <unistd.h>
+#define SIZE 8
+
+int opt, threadsprod, threadsconso;
+
+
+int main(int argc, char ** argv) {
+
+  while ((opt = getopt(argc, argv, "p:c:")) != -1) {
+    switch (opt) {
+    case 'j':
+      threadsprod = atoi(optarg);
+      break;
+    case 'n':
+      threadsconso = atoi(optarg);
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-p threadsProd] [-c threadsConso]\n", argv[0]);
+    }
+  }
   
+ 
+  
+  return 0;
 }
