76d1bdd1a366a89a29995cd28209426ec24f71a8
jef.overweg <jef.overweg@student.vinci.be>
2022-12-06 12:51:18 +0100

    commit philo

--- philosophes.c
+++ philosophes.c
@@ -1,52 +1,52 @@
 #include <pthread.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <stdbool.h>
+
 #include <unistd.h>
 
 #define CYCLES 100000
 
 int philosophes;
 
-void mange(int id) {}
+// Declare the threads, mutexes and philosopher IDs as global variables
+pthread_t phil[];
+pthread_mutex_t baguette[];
+int philosopher_ids[];
 
-void *philosophe(void *arg)
-{
-  pthread_t phil[philosophes];
-  pthread_mutex_t baguette[philosophes];
-  int *id = (int *)arg;
-  int left = *id;
+void mange(int id) {
+  printf("Philosophe %d mange\n", id);
+}
+
+void * philosophe(void * arg) {
+  // Get the philosopher ID from the argument
+  int id = * ((int * ) arg);
+  int left = id;
   int right = (left + 1) % philosophes;
   int i = 0;
-  while (i < CYCLES)
-  {
-    if (left < right)
-    {
-      pthread_mutex_lock(&baguette[left]);
-      pthread_mutex_lock(&baguette[right]);
+  while (i < CYCLES) {
+    // Check if the mutex is already locked before trying to lock it
+    if (pthread_mutex_trylock( & baguette[left]) == 0) {
+      if (pthread_mutex_trylock( & baguette[right]) == 0) {
+        mange(id);
+        pthread_mutex_unlock( & baguette[left]);
+        pthread_mutex_unlock( & baguette[right]);
+      }
     }
-    else
-    {
-      pthread_mutex_lock(&baguette[right]);
-      pthread_mutex_lock(&baguette[left]);
-    }
-    mange(*id);
-    pthread_mutex_unlock(&baguette[left]);
-    pthread_mutex_unlock(&baguette[right]);
     i++;
   }
   return (NULL);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char ** argv) {
   // Args handling
   int opt;
   int nthreads;
-  while ((opt = getopt(argc, argv, "j:n:")) != -1)
-  {
-    switch (opt)
-    {
+  while ((opt = getopt(argc, argv, "j:n:")) != -1) {
+    switch (opt) {
     case 'j':
       nthreads = atoi(optarg);
       break;
@@ -58,13 +58,21 @@ int main(int argc, char **argv)
     }
   }
 
-  pthread_t phil[philosophes];
-  pthread_mutex_t baguette[philosophes];
+  // Initialize the arrays to be the same size as nthreads
+  pthread_t phil[nthreads];
+  pthread_mutex_t baguette[nthreads];
+  int philosopher_ids[nthreads];
+
+  // Initialize the mutexes and philosopher IDs
+  for (int i = 0; i < nthreads; i++) {
+    pthread_mutex_init( & baguette[i], NULL);
+    philosopher_ids[i] = i;
+  }
 
   // Creating threads
   for (int i = 0; i < nthreads; i++)
-    pthread_create(&phil[i], NULL, philosophe, (void *)&i);
-  // Joinging threads
+    pthread_create( & phil[i], NULL, philosophe, (void * ) & philosopher_ids[i]);
+  // Joining threads
   for (int i = 0; i < nthreads; i++)
     pthread_join(phil[i], NULL);
 
