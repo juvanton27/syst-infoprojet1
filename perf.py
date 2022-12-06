48d74819047b46411b245a0edefe5a57f1cd5968
Julien Van Tongerloo <julien.vantongerloo@intuitim.com>
2022-12-06 15:04:03 +0100

    refactor - perf.py

--- perf.py
+++ perf.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 filename = input("Enter filename ('q' to quit) : ")
 while filename != 'q':
-
+  # Doing simple graph
   df = pd.read_csv(f'data/{filename}.csv', index_col=0)
   mean = df.mean(axis=1)
   std = df.std(axis=1)
@@ -14,9 +15,34 @@ while filename != 'q':
   plt.legend(title='legend')
   plt.ylabel('Temps (en secondes)')
   plt.xlabel('Nombre de threads')
-  plt.title(f'Test de performance de "{filename}.c"')
+  plt.title(f'Test de performance de "{filename}"')
 
   plt.savefig(f'images/{filename}.png')
-  plt.show()
+  plt.clf()
+
+  # Doing comparative graph
+  df = []
+  mean = []
+  index = 0
+  colors = ['red', 'blue', 'green', 'yellow']
+  for file in os.listdir('data/'):
+    if file.startswith(filename):
+      df.append({'data': pd.read_csv(f'data/{file}', index_col=0), 'filename': file, 'color': colors[index]})
+      index+=1
+
+  for data in df:
+    mean.append({'data': data['data'].mean(axis=1), 'filename': data['filename'], 'color': data['color']})
+
+  for m in mean:
+    plt.plot(m['data'], label=f"mean of {m['filename']}", color=m['color'])
+
+    plt.legend(title='legend')
+    plt.ylabel('Temps (en secondes)')
+    plt.xlabel('Nombre de threads')
+    plt.title(f'Comparatif des tests de performance de "{filename}"')
+
+  plt.savefig(f'images/full-{filename}.png')
+  plt.clf()
 
   filename = input("Enter filename ('q' to quit) : ")
+
